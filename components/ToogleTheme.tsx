1ea4260ac758616df6ff4b36c9a95fdf9a57d1b2
PATEL96 <patel.raj.official.96@gmail.com>
2024-07-23 14:57:21 +0530

    feat: added SHADCN UI and Dual Theme Mode

--- components/ToogleTheme.tsx
+++ components/ToogleTheme.tsx
@@ -3,35 +3,35 @@ import { GearIcon } from '@radix-ui/react-icons';
 import { useEffect, useState } from 'react';
 
 const ThemeToggle = () => {
-  const [theme, setTheme] = useState('light');
+	const [theme, setTheme] = useState('light');
 
-  useEffect(() => {
-    if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
-      document.documentElement.classList.add('dark');
-      setTheme('dark');
-    } else {
-      document.documentElement.classList.remove('dark');
-      setTheme('light');
-    }
-  }, []);
+	useEffect(() => {
+		if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+			document.documentElement.classList.add('dark');
+			setTheme('dark');
+		} else {
+			document.documentElement.classList.remove('dark');
+			setTheme('light');
+		}
+	}, []);
 
-  const toggleTheme = () => {
-    if (theme === 'dark') {
-      document.documentElement.classList.remove('dark');
-      localStorage.setItem('theme', 'light');
-      setTheme('light');
-    } else {
-      document.documentElement.classList.add('dark');
-      localStorage.setItem('theme', 'dark');
-      setTheme('dark');
-    }
-  };
+	const toggleTheme = () => {
+		if (theme === 'dark') {
+			document.documentElement.classList.remove('dark');
+			localStorage.setItem('theme', 'light');
+			setTheme('light');
+		} else {
+			document.documentElement.classList.add('dark');
+			localStorage.setItem('theme', 'dark');
+			setTheme('dark');
+		}
+	};
 
-  return (
-    <button onClick={toggleTheme}>
-      <GearIcon />
-    </button>
-  );
+	return (
+		<button onClick={toggleTheme}>
+			<GearIcon />
+		</button>
+	);
 };
 
 export default ThemeToggle;
